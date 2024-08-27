c93ee6ee8e03d68a09ca858cd14cc2337da3e100
zhukeyun <151515003+zhukeyun@users.noreply.github.com>
2024-08-28 02:14:21 +0800

    Update predict-top10.sh

--- predict-top10.sh
+++ predict-top10.sh
@@ -9,8 +9,8 @@ INPUT_FILE=$3
 OUT_FILE1=$OUT_PATH'/SOM.txt'
 OUT_FILE2=$OUT_PATH'/metabolite.txt'
 OUT_FILE3=$OUT_PATH'/predict.csv'
-onmt_translate -model ./model/SoM_identifier/mode1.pt ./model/SoM_identifier/mode2.pt ./model/SoM_identifier/mode3.pt ./model/SoM_identifier/mode4.pt -src $SRC_FILE -output $OUT_FILE1 -n_best 5 -beam_size 6 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
-onmt_translate -model ./model/metabolite_predictor/mode1.pt ./model/metabolite_predictor/mode2.pt ./model/metabolite_predictor/mode3.pt ./model/metabolite_predictor/mode4.pt ./model/metabolite_predictor/mode5.pt -src $OUT_FILE1 -output $OUT_FILE2 -n_best 2 -beam_size 8 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
-python process_predictions-predict.py -input_file $INPUT_FILE -predictions_file $OUT_FILE2 -output_file $PRED_FILE3 -predict_number 10
+onmt_translate -model ./model/SoM_identifier/model1.pt ./model/SoM_identifier/model2.pt ./model/SoM_identifier/model3.pt ./model/SoM_identifier/model4.pt -src $SRC_FILE -output $OUT_FILE1 -n_best 5 -beam_size 6 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
+onmt_translate -model ./model/metabolite_predictor/model1.pt ./model/metabolite_predictor/model2.pt ./model/metabolite_predictor/model3.pt ./model/metabolite_predictor/model4.pt ./model/metabolite_predictor/model5.pt -src $OUT_FILE1 -output $OUT_FILE2 -n_best 2 -beam_size 8 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
+python process_predictions-predict.py -input_file $INPUT_FILE -predictions_file $OUT_FILE2 -output_file $OUT_FILE3 -predict_number 10 -visualise_molecules True
 
 
