3908816235b943ad574adef412f3244a0413415e
zhukeyun <151515003+zhukeyun@users.noreply.github.com>
2024-08-28 02:20:24 +0800

    Update predict-top5.sh

--- predict-top5.sh
+++ predict-top5.sh
@@ -11,6 +11,5 @@ OUT_FILE2=$OUT_PATH'/metabolite.txt'
 OUT_FILE3=$OUT_PATH'/predict.csv'
 onmt_translate -model ./model/SoM_identifier/model1.pt ./model/SoM_identifier/model2.pt ./model/SoM_identifier/model3.pt ./model/SoM_identifier/model4.pt -src $SRC_FILE -output $OUT_FILE1 -n_best 5 -beam_size 8 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
 onmt_translate -model ./model/metabolite_predictor/model1.pt ./model/metabolite_predictor/model2.pt ./model/metabolite_predictor/model3.pt ./model/metabolite_predictor/model4.pt ./model/metabolite_predictor/model5.pt -src $OUT_FILE1 -output $OUT_FILE2 -n_best 1 -beam_size 3 -verbose -min_length 5 -max_length 150 -replace_unk -seed 42 -gpu 0
-python process_predictions-predict.py -input_file $INPUT_FILE -predictions_file $OUT_FILE2 -output_file $OUT_FILE3 -predict_number 5 -visualise_molecules True
-
+python process_predictions.py -input_file $INPUT_FILE -predictions_file $OUT_FILE2 -output_file $OUT_FILE3 -predict_number 5 -visualise_molecules True
 
